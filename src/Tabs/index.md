ef34237e809f5dbb231fc00c519ee335460b2780
sixgramwater <sixgramwater@126.com>
2021-06-24 15:55:54 +0800

    Enhance tab inkbar animation effect.

--- src/Tabs/index.md
+++ src/Tabs/index.md
@@ -10,7 +10,7 @@ const App = () => {
     <>
       <Tabs>
         <Tabs.TabItem label="选项卡一">选项卡一的内容</Tabs.TabItem>
-        <Tabs.TabItem label="选项卡二">选项卡二的内容</Tabs.TabItem>
+        <Tabs.TabItem label="选项卡二内容有点长">选项卡二的内容</Tabs.TabItem>
         <Tabs.TabItem label="选项卡三">选项卡三的内容</Tabs.TabItem>
       </Tabs>
     </>
--- src/Tabs/tabs.tsx
+++ src/Tabs/tabs.tsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useRef, useEffect } from 'react';
 import classNames from 'classnames';
 import { TabItemProps } from './tabItem';
 import TabNav from './tabNav';
@@ -27,7 +27,34 @@ const Tabs: React.FC<TabsProps> = (props) => {
   const { type, className, defaultIndex, onSelect, children } = props;
 
   const [activeIndex, setActive] = useState(defaultIndex);
+  const [offset, setOffset] = useState<any>([]);
+  // const [inkbarOffset, setOffset] = useState(0);
   const classes = classNames(`liquid-tabs tabs-${type}`, className);
+  const tabNavRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  // 获取所有tabNav的DOM，再记录在一个数组中
+  useEffect(() => {
+    if (tabNavRef.current) {
+      // console.log(tabNavRef.current.children)
+      setTimeout(() => {
+        let elements = [...tabNavRef.current.children] as HTMLElement[];
+        console.log(elements);
+        setOffset(
+          elements.map((el) => {
+            return {
+              left: el.offsetLeft,
+              width: el.clientWidth,
+            };
+          }),
+        );
+      }, 300);
+      // let elements = [...tabNavRef.current.children];
+      // console.log(elements)
+      // console.log(nodes);
+      // setOffset(nodes.map(elem=>elem.offsetLeft));
+      // console.log(offset);
+    }
+  }, [tabNavRef]);
   const handleClick = (index: string) => {
     setActive(index);
     if (onSelect) {
@@ -38,6 +65,21 @@ const Tabs: React.FC<TabsProps> = (props) => {
     index: activeIndex ? activeIndex : '0',
     onSelect: handleClick,
   };
+  const calInkBarStyle = () => {
+    const index = Number(activeIndex);
+    if (offset[index]) {
+      return {
+        left: offset[index].left + 'px',
+        width: offset[index].width + 'px',
+      };
+    } else {
+      return {
+        left: '0',
+        width: '0',
+      };
+    }
+  };
+  // legacy draft(Out-of-date)
   const inkbarStyle = {
     left: activeIndex ? parseInt(activeIndex) * (56 + 32) + 'px' : '0',
   };
@@ -64,9 +106,9 @@ const Tabs: React.FC<TabsProps> = (props) => {
     return (
       <>
         <div className="tabs-bar">
-          <div className="tabs-nav-container">
+          <div className="tabs-nav-container" ref={tabNavRef}>
             {tabsNavItems}
-            <div className="tab-ink-bar" style={inkbarStyle}></div>
+            <div className="tab-ink-bar" style={calInkBarStyle()}></div>
           </div>
         </div>
         <div className="tabs-content">{tabsContentItems}</div>
